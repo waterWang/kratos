a0629396e061de19485cf5eab1401a4aab929e33
water <672684719@qq.com>
2026-08-08 07:40:42 +0800

    fix: inject CLI version into project package for template version matching

--- cmd/kratos/main.go
+++ cmd/kratos/main.go
@@ -20,6 +20,7 @@ var rootCmd = &cobra.Command{
 }
 
 func init() {
+	project.CLIVersion = release
 	rootCmd.AddCommand(project.CmdNew)
 	rootCmd.AddCommand(proto.CmdProto)
 	rootCmd.AddCommand(upgrade.CmdUpgrade)
